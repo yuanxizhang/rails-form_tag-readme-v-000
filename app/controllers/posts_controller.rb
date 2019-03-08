8114dfdc90b8e7c412c19cc221cfa179b72bd9d9
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-08 04:15:50 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-  end
+		@posts = Post.all
+	end
+
+	def new 
+
+	end
+
+	def show  
+		@post = Post.find(params[:id])
+	end
+
+	def create
+	  Post.create(title: params[:post][:title], description: params[:post][:description])
+	  redirect_to posts_path
+	end
   
 end
