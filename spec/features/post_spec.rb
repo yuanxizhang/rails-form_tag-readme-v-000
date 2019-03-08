35abc73656b0884aeb82c61de37f79f5313c07fd
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-08 04:18:38 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe 'new post' do
+  it 'renders HTML in the /new template' do
+    visit new_post_path
+    expect(page).to have_content('Post Form')
+  end
+  
+  
 
 end
